4ff17dc8bcf5d453e46f44ff71ac1243cfef6779
writerpass <mail@writerpass.top>
2025-09-13 16:22:48 +0800

    update

--- QWeatherKMP/src/commonMain/kotlin/top/writerpass/qweather/QWeatherRequestResult.kt
+++ QWeatherKMP/src/commonMain/kotlin/top/writerpass/qweather/QWeatherRequestResult.kt
@@ -1,9 +1,11 @@
 package top.writerpass.qweather
 
 import io.ktor.http.HttpStatusCode
+import kotlinx.serialization.json.JsonObject
 
 data class QWeatherRequestResult(
     val statusCode: HttpStatusCode,
     val data: Any,
     val notImplemented: Boolean = data is String
 )
+
