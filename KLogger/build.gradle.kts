14cbbcc46b663ce67ba5ac9e35dae552f9f62053
WriterPass <mail@writerpass.top>
2025-09-21 15:21:55 +0800

    update

--- KLogger/build.gradle.kts
+++ KLogger/build.gradle.kts
@@ -4,19 +4,19 @@ plugins {
 
 kotlin {
     jvm()
-    wasmJs()
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-        macosX64(),
-        macosArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
+//    wasmJs()
+//    listOf(
+//        iosX64(),
+//        iosArm64(),
+//        iosSimulatorArm64(),
+//        macosX64(),
+//        macosArm64()
+//    ).forEach {
+//        it.binaries.framework {
+//            baseName = "ComposeApp"
+//            isStatic = true
+//        }
+//    }
 
     sourceSets {
         val commonMain by getting {
