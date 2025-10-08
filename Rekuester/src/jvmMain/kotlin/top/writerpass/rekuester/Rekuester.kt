8a25040bd82270bdedfee64acaa0c0cfee04dc2d
writerpass <mail@writerpass.top>
2025-10-08 18:57:55 +0800

    update

--- Rekuester/src/jvmMain/kotlin/top/writerpass/rekuester/Rekuester.kt
+++ Rekuester/src/jvmMain/kotlin/top/writerpass/rekuester/Rekuester.kt
@@ -1,12 +1,42 @@
 package top.writerpass.rekuester
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.singleWindowApplication
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import io.ktor.http.HttpMethod
 import kotlinx.serialization.Serializable
+import top.writerpass.cmplibrary.compose.FullSizeColumn
+import top.writerpass.cmplibrary.compose.FullWidthRow
 import top.writerpass.rekuester.data.dao.ItemWithId
 import top.writerpass.rekuester.ui.ApplicationTray
 import top.writerpass.rekuester.ui.window.CollectionManagerWindow
@@ -95,7 +125,7 @@ data class Collection(
 // TODO 增加自定义Body的编辑功能
 // TODO 使用Navigation作为导航
 
-fun main() {
+fun main1() {
     application {
         CompositionLocalProvider(
             LocalViewModelStoreOwner provides Singletons.viewModelStoreOwner,
@@ -118,6 +148,134 @@ fun main() {
                 CollectionManagerWindow()
                 MainWindow()
             }
+        }
+    }
+}
+
+fun main() = singleWindowApplication {
+    CMPTable1(
+        listOf(
+            listOf("AA","BB","CC","DD","EE","FF"),
+            listOf("AA","BB","CC","DD","EE","FF"),
+            listOf("AA","BB","CC","DD","EE","FF"),
+            listOf("AA","BB","CC","DD","EE","FF"),
+            listOf("AA","BB","CC","DD","EE","FF"),
+        )
+    )
+}
+
+
+@Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float
+) {
+    VerticalDivider()
+    androidx.compose.material3.Text(
+        text = text,
+        modifier = Modifier
+//            .border(width = 1.dp, color =  Color.Black)
+            .weight(weight)
+            .padding(8.dp)
+    )
+}
+
+class ColumnState(default: Int = 120){
+    var width by mutableIntStateOf(default)
+}
+
+class RowState(default: Int = 60){
+    var height by mutableIntStateOf(default)
+}
+
+class CellState(
+    val columnState: ColumnState,
+    val rowState: RowState
+)
+
+@Composable
+fun LazyItemScope.TabRow(
+    rowState: RowState
+){
+    HorizontalDivider()
+    Row(modifier = Modifier.background(Color.Gray).height(rowState.height.dp)) {
+        TableCell({})
+        TableCell({})
+        VerticalDivider()
+    }
+}
+
+//object CellStyleHolder{
+//    private val rowStateMap = mutableMapOf<Int, RowState>()
+//    private val columnStateMap = mutableMapOf<Int, ColumnState>()
+//
+//    fun getState(x:Int,y:Int): CellState{
+//
+//    }
+//}
+
+@Composable
+fun CMPTable1(
+    dataSet:List<List<Any>>
+){
+    val defaultWidth = 120.dp
+    val defaultHeight = 40.dp
+    Column {
+        HorizontalDivider()
+        dataSet.forEach { dataRow->
+            FullWidthRow(modifier = Modifier.height(defaultHeight)) {
+                VerticalDivider()
+                dataRow.forEach { data->
+                    val dateValue = remember { data.toString() }
+                    Text(dateValue, modifier = Modifier.width(defaultWidth).fillMaxHeight())
+                    VerticalDivider()
+                }
+            }
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun RowScope.TableCell(
+    content: @Composable RowScope.() -> Unit
+) {
+    VerticalDivider()
+    content()
+}
+
+
+@Composable
+fun TableScreen() {
+    // Just a fake data... a Pair of Int and String
+    val tableData = (1..100).mapIndexed { index, item ->
+        index to "Item $index"
+    }
+    // Each cell of a column must have the same weight.
+    val column1Weight = .3f // 30%
+    val column2Weight = .7f // 70%
+    // The LazyColumn will be our table. Notice the use of the weights below
+    LazyColumn(Modifier.fillMaxSize().padding(16.dp)) {
+        // Here is the header
+        item {
+            HorizontalDivider()
+            Row(modifier = Modifier.background(Color.Gray).height(IntrinsicSize.Min)) {
+                TableCell(text = "Column 1", weight = column1Weight)
+                TableCell(text = "Column 2", weight = column2Weight)
+                VerticalDivider()
+            }
+        }
+        // Here are all the lines of your table.
+        items(tableData) { (id, text) ->
+            HorizontalDivider()
+            Row(Modifier.fillMaxWidth().height(IntrinsicSize.Min)) {
+                TableCell(text = id.toString(), weight = column1Weight)
+                TableCell(text = text, weight = column2Weight)
+                VerticalDivider()
+            }
+        }
+        item {
+            HorizontalDivider()
         }
     }
 }
--- Rekuester/src/jvmMain/kotlin/top/writerpass/rekuester/ui/page/ApiRequestPage.kt
+++ Rekuester/src/jvmMain/kotlin/top/writerpass/rekuester/ui/page/ApiRequestPage.kt
@@ -1,10 +1,17 @@
 package top.writerpass.rekuester.ui.page
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -22,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import io.ktor.http.HttpMethod
